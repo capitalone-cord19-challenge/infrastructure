a2fd127ee9cc4cbba9906946e78f72e4f93e6f3d
Dan Kerrigan <dan.kerrigan@gmail.com>
2020-04-15 12:20:29 -0400

    Add worker policy allowing autoscaling permissions mitigating crash backoff loop

--- aws/main.tf
+++ aws/main.tf
@@ -57,6 +57,55 @@ module "vpc" {
   }
 }
 
+resource "aws_iam_policy" "worker_autoscaling" {
+  name_prefix = "eks-worker-autoscaling-${module.eks.cluster_id}"
+  description = "EKS worker node autoscaling policy for cluster ${module.eks.cluster_id}"
+  policy      = data.aws_iam_policy_document.worker_autoscaling.json
+  path        = "/"
+}
+
+data "aws_iam_policy_document" "worker_autoscaling" {
+  statement {
+    sid    = "eksWorkerAutoscalingAll"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "ec2:DescribeLaunchTemplateVersions",
+    ]
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "eksWorkerAutoscalingOwn"
+    effect = "Allow"
+
+    actions = [
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "autoscaling:UpdateAutoScalingGroup",
+    ]
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/kubernetes.io/cluster/${module.eks.cluster_id}"
+      values   = ["owned"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "autoscaling:ResourceTag/k8s.io/cluster-autoscaler/enabled"
+      values   = ["true"]
+    }
+  }
+}
+
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
   cluster_name = var.cluster_name
@@ -111,6 +160,10 @@ module "eks" {
   #   # FIXME: Narrow these permissions down?
   #   groups   = ["system:masters"]
   # }], var.map_users)
+
+  workers_additional_policies = [
+    aws_iam_policy.worker_autoscaling.arn
+  ]
 }
 
 # resource "aws_iam_user" "hubploy_eks_user" {
